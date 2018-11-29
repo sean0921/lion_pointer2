f5348946b9ce7be71e3b3899f9e4def2a9454494
sean0921 <sean.li.shin.ho@gmail.com>
2018-11-29 20:05:43 +0800

    chmod 644

--- pointer_vs_array.c
+++ pointer_vs_array.c
@@ -6,7 +6,7 @@ int main(){
 	char *b="b string";
 	strcat(a," can change\n");
 	printf("%s",a);
-	//strcat(b," can change\n");//Segmentation fault
+	strcat(b," can change\n");//Segmentation fault
 	printf("%s",b);
 	return 0;
 }
